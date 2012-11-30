c5e3d87450ff48142b413e6518ece854674dc257
Basel Farah <baself@gmail.com>
2012-11-30 14:40:08 +0000

    added cosm gem

--- Rakefile
+++ Rakefile
@@ -1,5 +1,3 @@
 task :default do
 	require File.expand_path(File.join(*%w[ config environment ]), File.dirname(__FILE__))
 end
-
-bundle exec ruby web_test.rb
--- web.rb
+++ web.rb
@@ -1,12 +1,27 @@
 require 'sinatra'
 require 'json'
+require 'cosm-rb'
 
 get '/' do
   "Travis to Cosm"
 end
 
 post '/notifications' do
-	content_type :json
   data = JSON.parse(request.body.read)
-	puts data.inspect
+
+  config = YAML.load_file('config/cosm.yml')
+  API_KEY = config["api_key"]
+  FEED_ID = config["feed_id"]
+
+  repository = data["repository"]["name"]
+  status = data["status_message"] == "Passed" ? 1 : 0
+
+  { repository.to_sym => status }.each_pair do |key,value|
+      datapoint = Cosm::Datapoint.new(:at => Time.now, :value => value)
+      Cosm::Client.post("/v2/feeds/#{FEED_ID}/datastreams/#{key}/datapoints",
+        :headers => {"X-ApiKey" => API_KEY},
+        :body => {:datapoints => [datapoint]}.to_json)
+  end
+
+
 end
